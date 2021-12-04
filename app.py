4e860e18fe9b083028f89463ae225572d3dc9458
maxylink <86682292+maxylink@users.noreply.github.com>
2021-12-04 14:56:23 -0800

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ sleep(3)
 
 
 email=driver.find_element_by_id("email")
-email.send_keys("100054592715360")
+email.send_keys("doniben.dominic")
 password=driver.find_element_by_id("pass")
-password.send_keys("chicheta")
+password.send_keys("maxylinks")
 sleep(1)
 login=driver.find_element_by_name("login")
 login.click()
@@ -44,15 +44,17 @@ sleep(4)
 
 
 
+i = 1
+
+while i<=50000:
 
 
-
-groups_links_list = [
+ groups_links_list = [
    "https://www.facebook.com/groups/3686706161407106", "https://www.facebook.com/groups/2782001165164383", "https://www.facebook.com/groups/2020733191524486", "https://www.facebook.com/groups/5440056676034888", "https://www.facebook.com/groups/internationalcryptominers", "https://www.facebook.com/groups/1977782575833348", "https://www.facebook.com/groups/753677805258668", "https://www.facebook.com/groups/423287535042518", "https://www.facebook.com/groups/1953339638311954", "https://www.facebook.com/groups/818536955169151", "https://www.facebook.com/groups/755878041998722", "https://www.facebook.com/groups/753677805258668", "https://www.facebook.com/groups/751840511663201", "https://www.facebook.com/groups/316292243440591", "https://www.facebook.com/groups/1088619671559643", "https://www.facebook.com/groups/387055385984329", "https://www.facebook.com/groups/341683990228113", "https://www.facebook.com/groups/716910665175389", "https://www.facebook.com/groups/cryptocurrencybitcoininvestment", "https://www.facebook.com/groups/newsbitcoinworld", "https://www.facebook.com/groups/usabusinessgroupz", "https://www.facebook.com/groups/2782001165164383", "https://www.facebook.com/groups/2438045376224715", "https://www.facebook.com/groups/2020733191524486", "https://www.facebook.com/groups/1893338250702751", "https://www.facebook.com/groups/internationalcryptominers", "https://www.facebook.com/groups/1829098377306709", "https://www.facebook.com/groups/UcoincashIndonesiaRaya", "https://www.facebook.com/groups/1709996879303044", "https://www.facebook.com/groups/1572466832810159", "https://www.facebook.com/groups/bitcoinworldtm", "https://www.facebook.com/groups/1142220402650653", "https://www.facebook.com/groups/binancecom", "https://www.facebook.com/groups/insidecrypto.news", "https://www.facebook.com/groups/germanbtc", "https://www.facebook.com/groups/595862454626204", "https://www.facebook.com/groups/530659007137021", "https://www.facebook.com/groups/1130482460326432", "https://www.facebook.com/groups/1894175397292966", "https://www.facebook.com/groups/hyip.raj.2"
     ]
 
  
-for group in groups_links_list:
+ for group in groups_links_list:
     driver.get(group)
     sleep(7)
        
@@ -75,7 +77,7 @@ for group in groups_links_list:
     sleep(2)
     messages = ['Many investors have lost all their investment profits because they don not believe in reality ,\nwhile some lost their major withdrawals to fake companies and many are still loosing till now.\nI am here to introduce you all to the greatest paying and legitimate investment company of our time..I have been receiving my withdrawals automatic and I am so glad to witness the uprising of my daily investment with Zend Cryptos Investments platform.\nBelieve me this is the best platform to invest with now.\nRegister and strat earning like me now.Follow the link now.\nhttp://www.google.com/search?q=site%3Azendforex.com','great investment platform today ']
     for message in messages:
-      element.send_keys(message)
+     element.send_keys(message)
    
     
     
@@ -120,3 +122,4 @@ for group in groups_links_list:
     
     
 
+i=i+1
