c9c45504904864268e0691aa265492d3bd585ecb
maxylink <86682292+maxylink@users.noreply.github.com>
2021-12-08 00:08:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -93,31 +93,7 @@ while i<=50000:
     sleep(2)
     
     
-    driver.get("https://facebook.com/")
-    sleep(5) 
-    driver.find_element_by_xpath("//div[@aria-label='Account']//*[name()='svg']").click()
-    sleep(4)
-    driver.find_element_by_xpath("//span[normalize-space()='Log Out']").click()
-
-    sleep(2)
-
-    
-    
-    driver.get("https://facebook.com/")
-    sleep(1)
-
-    email=driver.find_element_by_id("email")
-    email.send_keys("100054592715360")
-    password=driver.find_element_by_id("pass")
-    password.send_keys("chicheta5")
-    sleep(1)
-    login=driver.find_element_by_name("login")
-    login.click()
-    sleep(4)
-
-
-    
-    sleep(1)
+  
 
     
 i=i+1  
